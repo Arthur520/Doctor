049a4d321b705ee31e5eda9f4c5bd34dd8d6285b
Arthur <1549935001@qq.com>
2020-03-22 22:35:56 +0800

    编写发现界面
    主要实现文章分类

--- pages/find/find.js
+++ pages/find/find.js
@@ -1,4 +1,5 @@
 // pages/find/find.js
+var timer = require("../../utils/time.js");
 const app = getApp();
 Page({
 
@@ -11,7 +12,37 @@ Page({
     currentTab: 0,
     firstArticleList:null,
     secondArticleList: null,
-    thirdArticleList: null
+    thirdArticleList: null,
+    select1: false,
+    select2:false,
+    select3:false,
+    department1: "全部内科",
+    department2:"全部外科",
+    department3: "其他全部"
+  },
+  mySelect1(e) {
+    var name = e.currentTarget.dataset.name
+    this.setData({
+      department1: name
+    })
+    this.onShow();
+    console.log(this.data.department1)
+  },
+  mySelect2(e) {
+    var name = e.currentTarget.dataset.name
+    this.setData({
+      department2: name
+    })
+    this.onShow();
+    console.log(this.data.department2)
+  },
+  mySelect3(e) {
+    var name = e.currentTarget.dataset.name
+    this.setData({
+      department3: name
+    })
+    this.onShow();
+    console.log(this.data.department3)
   },
   bindChange: function (e) {
     var that = this;
@@ -22,12 +53,43 @@ Page({
   //滑动切换
   swichNav: function (e) {
     var that = this;
-    if (this.data.currentTab === e.target.dataset.current) {
+    if (this.data.currentTab == e.target.dataset.current) {
+      if (this.data.currentTab==0){
+      that.setData({
+      select1: !this.data.select1
+      })}
+      if (this.data.currentTab == 1) {
+        that.setData({
+          select2: !this.data.select2
+        })
+      }
+      if (this.data.currentTab == 2) {
+        that.setData({
+          select3: !this.data.select3
+        })
+      }
       return false;
     } else {
-      that.setData({
-        currentTab: e.target.dataset.current
-      })
+      if (this.data.currentTab == 0) {
+        that.setData({
+          currentTab: e.target.dataset.current,
+          select1: false
+        })
+      }
+      if (this.data.currentTab == 1) {
+        that.setData({
+          currentTab: e.target.dataset.current,
+          select2: false
+        })
+      }
+      if (this.data.currentTab == 2) {
+        that.setData({
+          currentTab: e.target.dataset.current,
+          select3: false
+        })
+      }
+      
+      
     }
   },
   //点击切换
@@ -58,6 +120,13 @@ Page({
           that.setData({
             firstArticleList: list
           });
+          var datelist1 = new Array();
+          for (var i = 0; i < list.length; i++) {
+            datelist1[i] = timer.js_date_another_time(that.data.firstArticleList[i].date); 
+          }
+          that.setData({
+            datelist1: datelist1
+          })
         } else {
           // 失败弹出框
           wx.showToast({
@@ -83,6 +152,13 @@ Page({
           that.setData({
             secondArticleList: list
           });
+          var datelist2 = new Array();
+          for (var i = 0; i < list.length; i++) {
+            datelist2[i] = timer.js_date_another_time(that.data.secondArticleList[i].date); 
+          }
+          that.setData({
+            datelist2: datelist2
+          })
         } else {
           // 失败弹出框
           wx.showToast({
@@ -108,6 +184,13 @@ Page({
           that.setData({
             thirdArticleList: list
           });
+          var datelist3 = new Array();
+          for (var i = 0; i < list.length; i++) {
+            datelist3[i] = timer.js_date_another_time(that.data.thirdArticleList[i].date);
+          }
+          that.setData({
+            datelist3: datelist3
+          })
         } else {
           // 失败弹出框
           wx.showToast({
@@ -140,7 +223,113 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var serverUrl = app.globalData.serverUrl;
+    var that = this;
+    wx.request({
+      url: serverUrl + '/article/changefirsttype',
+      method: "POST",
+      data: {
+        type: that.data.department1,
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        var data = res.data;
+        console.log(res);
+        var list = data.data;
+        console.log(list);
+        if (data.status == 200) {
+          that.setData({
+            firstArticleList: list
+          });
+          var datelist1 = new Array();
+          for (var i = 0; i < list.length; i++) {
+            datelist1[i] = timer.js_date_another_time(that.data.firstArticleList[i].date);
+          }
+          that.setData({
+            datelist1: datelist1
+          })
+        } else {
+          // 失败弹出框
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+            duration: 3000
+          })
+        }
+      }
+    })
+    wx.request({
+      url: serverUrl + '/article/changesecondtype',
+      method: "POST",
+      data: {
+        type: that.data.department2,
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        var data = res.data;
+        console.log(res);
+        var list = data.data;
+        console.log(list);
+        if (data.status == 200) {
+          that.setData({
+            secondArticleList: list
+          });
+          var datelist2 = new Array();
+          for (var i = 0; i < list.length; i++) {
+            datelist2[i] = timer.js_date_another_time(that.data.secondArticleList[i].date);
+          }
+          that.setData({
+            datelist2: datelist2
+          })
+        } else {
+          // 失败弹出框
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+            duration: 3000
+          })
+        }
+      }
+    })
+    wx.request({
+      url: serverUrl + '/article/changethirdtype',
+      method: "POST",
+      data: {
+        type: that.data.department3,
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        var data = res.data;
+        console.log(res);
+        var list = data.data;
+        console.log(list);
+        if (data.status == 200) {
+          that.setData({
+            thirdArticleList: list
+          });
+          var datelist3 = new Array();
+          for (var i = 0; i < list.length; i++) {
+            datelist3[i] = timer.js_date_another_time(that.data.thirdArticleList[i].date);
+          }
+          that.setData({
+            datelist3: datelist3
+          })
+        } else {
+          // 失败弹出框
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+            duration: 3000
+          })
+        }
+      }
+    })
   },
 
   /**
--- utils/time.js
+++ utils/time.js
@@ -27,6 +27,36 @@ function js_date_time(unixtime) {
   var timeSpanStr = year + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second;
   return timeSpanStr;
 }
+function js_date_another_time(unixtime) {
+  var dateTime = new Date(parseInt(unixtime));//注意，此处unixtime是接收到的原生数据，不需要做任何处理，网上有些代码写的是需要乘以1000，亲测不需要乘以1000，注意注意！！！！！！！！
+  console.log(dateTime.getUTCFullYear());
+
+  var year = dateTime.getFullYear();
+  var month = (dateTime.getMonth() + 1 < 10 ? '0' + (dateTime.getMonth() + 1) : dateTime.getMonth() + 1);
+  var day = dateTime.getDate() < 10 ? dateTime.getDate() : dateTime.getDate();
+  var hour = dateTime.getHours();
+  var minute = dateTime.getMinutes();
+  var second = dateTime.getSeconds();
+  var now = new Date();
+  var now_new = Date.parse(now.toDateString()); //typescript转换写法
+  var milliseconds = now_new - dateTime;
+
+  if (day < 10) {
+    day = '0' + day;
+  }
+  if (hour < 10) {
+    hour = '0' + hour;
+  }
+  if (minute < 10) {
+    minute = '0' + minute;
+  }
+  if (second < 10) {
+    second = '0' + second;
+  }
+  var timeSpanStr = year + '/' + month + '/' + day;
+  return timeSpanStr;
+}
 module.exports = {
-  js_date_time: js_date_time
+  js_date_time: js_date_time,
+  js_date_another_time: js_date_another_time,
 }
